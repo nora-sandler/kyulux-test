780361c5fc871981af0333be40855bc678879252
Nora Sandler <gulnora.touraeva@gmail.com>
2021-03-02 01:34:22 -0500

    Made fetch request and started parsing the data.

--- src/Table.js
+++ src/Table.js
@@ -26,20 +26,20 @@ function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+let rows = [
+  // createData('Cupcake', 305, 3.7, 67, 4.3),
+  // createData('Donut', 452, 25.0, 51, 4.9),
+  // createData('Eclair', 262, 16.0, 24, 6.0),
+  // createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  // createData('Gingerbread', 356, 16.0, 49, 3.9),
+  // createData('Honeycomb', 408, 3.2, 87, 6.5),
+  // createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  // createData('Jelly Bean', 375, 0.0, 94, 0.0),
+  // createData('KitKat', 518, 26.0, 65, 7.0),
+  // createData('Lollipop', 392, 0.2, 98, 0.0),
+  // createData('Marshmallow', 318, 0, 81, 2.0),
+  // createData('Nougat', 360, 19.0, 9, 37.0),
+  // createData('Oreo', 437, 18.0, 63, 4.0),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -68,12 +68,61 @@ function stableSort(array, comparator) {
   return stabilizedThis.map((el) => el[0]);
 }
 
+function parseFDAAdverseEventSearch(adverseEventsResponse) {
+  console.log("parseFDAAdverseEventSearch")
+  console.log(adverseEventsResponse)
+  let ourRows = []
+  let results = adverseEventsResponse.results
+  for(let iresult = 0; iresult < results.length; iresult++) {
+
+
+    // Get reactions.
+    let symptoms = []
+
+    let reactions = results[iresult].patient.reaction
+    for(let ireaction = 0; ireaction < reactions.length; ireaction++) {
+      symptoms.push(reactions[ireaction].reactionmeddrapt)
+    }
+
+
+   // Get date
+   let date = "0"
+
+
+   // Get drugs
+   let drugs = "None"
+
+
+   // Get age
+   let age = "10"
+
+
+   // Get country
+   let country= "USA"
+
+   let curRow = createData(symptoms, date, drugs, age, country)
+   ourRows.push(curRow)
+  }
+
+  return ourRows
+}
+
+function errorReport(error)
+{
+  console.log("error")
+}
+
+
+fetch('https://api.fda.gov/drug/event.json?search=patient.reaction.reactionmeddrapt:%22headache%22&limit=50').then(response => response.json())
+            .then(data => { rows = parseFDAAdverseEventSearch(data)
+        }).catch((error) => { errorReport(error) })
+
 const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: 'reaction', numeric: false, disablePadding: true, label: 'Reactions' }, // patient.reaction.reactionmeddrapt
+  { id: 'date', numeric: false, disablePadding: false, label: 'Date' }, // receiptdate
+  { id: 'drugs', numeric: false, disablePadding: false, label: 'Drugs' }, // drug[].openfda.generic_name[]
+  { id: 'age', numeric: true, disablePadding: false, label: 'Age' }, // patientonsetage
+  { id: 'country', numeric: false, disablePadding: false, label: 'Country of Occurence' }, // occurcountry
 ];
 
 function EnhancedTableHead(props) {
